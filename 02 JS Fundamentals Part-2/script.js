c20e49aa999339b69a41e25b227aa513d8312d44
ualiurrahat <ualiurrahat25@gmail.com>
2024-04-05 02:30:30 +0600

    activated strict mode

---
+++ 02 JS Fundamentals Part-2/script.js
@@ -0,0 +1,18 @@
+// activationg strict mode
+// to activate strict mode, write the code:
+// "use strict" at the very fist line of the script file
+// otherwise it won't work for whole file.
+// "use strict" has to be the first line of code that will run
+// having comment above it will have no problem,just like these ones.
+"use strict";
+
+// strict mode helps us to find error in our script file.
+let hasDriverLicense = false;
+const passTest = true;
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) {
+  console.log("I can drive");
+}
+
+// const private = 25;
+// const if = 8;
