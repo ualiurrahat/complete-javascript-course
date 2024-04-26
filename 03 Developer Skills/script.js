30a98f079a445c84e5de728ab8d0dc413ef2711b
ualiurrahat <ualiurrahat25@gmail.com>
2024-04-26 20:46:27 +0600

    developer skills folder and necessary files added

---
+++ 03 Developer Skills/script.js
@@ -0,0 +1,8 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = '25';
+
+const age = birthyear => 2024 - birthyear;
+const myself = 'I am rahat';
+console.log(myself);
