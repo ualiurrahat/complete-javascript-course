3cf6af842eabf1270193debfa11fb6d4fae27026
ualiurrahat <ualiurrahat25@gmail.com>
2024-04-27 19:25:25 +0600

    handled click events

--- 04 Guess My Number/script.js
+++ 04 Guess My Number/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 console.log(document.querySelector('.message').textContent);
@@ -8,3 +8,15 @@ console.log(document.querySelector('.score').textContent);
 document.querySelector('.score').textContent = 10;
 document.querySelector('.number').textContent = 50;
 document.querySelector('.guess').value = 15;
+*/
+
+// handling clicke events
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  // handling case when no number is inserted
+  // in the input field
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  }
+});
