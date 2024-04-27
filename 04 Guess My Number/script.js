487ceab15ae050b8c25016057839dee61c8d3564
ualiurrahat <ualiurrahat25@gmail.com>
2024-04-27 19:06:45 +0600

    selected and manipulated elements

--- 04 Guess My Number/script.js
+++ 04 Guess My Number/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+console.log(document.querySelector('.score').textContent);
+document.querySelector('.score').textContent = 10;
+document.querySelector('.number').textContent = 50;
+document.querySelector('.guess').value = 15;
