1255db60cdc9e3ac29c93ab12001be49a08b439f
ualiurrahat <ualiurrahat25@gmail.com>
2024-04-30 12:39:11 +0600

    implemented high score

--- 04 Guess My Number/script.js
+++ 04 Guess My Number/script.js
@@ -15,6 +15,8 @@ let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 // to manage user's score
 let score = 20;
+// to manage high score
+let highScore = 0;
 // handling click events on the 'check' button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -36,6 +38,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
     // show the number in number plate
     document.querySelector('.number').textContent = secretNumber;
+    // update high score
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
   // case 3: when user guessed number is higher
   else if (guess > secretNumber) {
@@ -66,23 +73,21 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
-// coding challenge
-/*Implement a game rest functionality, so that the player can make a new guess!
-Your tasks:
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the 'score' and 
-'secretNumber' variables
-3. Restore the initial conditions of the message, number, score and guess input 
-fields
-4. Also restore the original background color (#222) and number width (15rem)
-*/
+
+// impelementing the game reset functionality
+//1. Select the element with the 'again' class and attach a click event handler
 document.querySelector('.again').addEventListener('click', function () {
+  // 2. In the handler function, restore initial values
+  // of the 'score' and'secretNumber' variables
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
+  //3. Restore the initial conditions of the message, number,
+  //   score and guess input fields;
   document.querySelector('.message').textContent = 'Start guessing ...';
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
+  //   4. Also restore the original background color (#222) and number width (15rem)
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
